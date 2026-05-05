acee9ea0ddfa209902a5c8ae18eebfa9e22b49bd
ByteEntropy <34189432+ByteEntropyDotCom@users.noreply.github.com>
2026-05-05 23:54:53 +0800

    Add files via upload

---
+++ discovery-server/src/main/java/com/byteentropy/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.byteentropy.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer // This turns the app into a Service Registry
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
---
+++ discovery-server/src/test/java/com/byteentropy/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,24 @@
+package com.byteentropy.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DiscoveryServerTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testEurekaDashboardLoads() {
+        ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).contains("System Status");
+    }
+}
